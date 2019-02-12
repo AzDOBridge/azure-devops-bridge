b61d1581141b2c5bc5cff80924b192872c7d506f
Modar Elias <melias@alegri.eu>
2019-02-12 14:09:50 +0100

    add unit tests

---
+++ test/AzDoBridgeUnitTests/ActionsTests/ActionsTests.cs
@@ -0,0 +1,32 @@
+using AzDoBridge.Actions;
+using AzDoBridge.Models;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using Xunit;
+using FluentAssertions;
+using Alexa.NET.Response;
+using Alexa.NET.Request;
+
+namespace AzDoBridgeUnitTests
+{
+    public class ActionsTests
+    {
+        Mock<ILogger> logger;
+        public ActionsTests()
+        {
+            logger = new Mock<ILogger>();
+        }
+        [Theory]
+        [InlineData(AzDoBridgeIntent.AssignTo)]
+        [InlineData(AzDoBridgeIntent.ChangeWiStatus)]
+        [InlineData(AzDoBridgeIntent.SetPriority)]
+        public void Should_Factor_Multiple_Actions(AzDoBridgeIntent azDoBridgeIntent)
+        {
+            IAzDoBridgeAction action;
+            var fac = AzDoBridgeActionFactory.TryGetAction(azDoBridgeIntent.ToString(), logger.Object, out action);
+            fac.Should().BeTrue();
+            action.GetType().Should().Equals(typeof(AzDoBridgeIntent));            
+        }
+    }
+}
---
+++ test/AzDoBridgeUnitTests/ModelsTests/ModelsTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using FluentAssertions;
+using Alexa.NET.Request;
+using AzDoBridgeUnitTests.Utilities;
+using Alexa.NET.Request.Type;
+using AzDoBridge.Models;
+
+namespace AzDoBridgeUnitTests.ModelsTests
+{
+    public class ModelsTests
+    {
+        const string testFilePath = "TestInputs";
+        const string testFile = "IntentRequest.json";
+        
+        [Theory]
+        [InlineData("itemid")]
+        [InlineData("itemstate")]
+        [InlineData("Fullname")]
+        public void Can_Get_SlotValue_For_Intent(string slotName)
+        {
+            SkillRequest skillRequest = TestUtilities.EntityFromFile<SkillRequest>(testFilePath, testFile);
+            var intentRequest = (IntentRequest)skillRequest.Request;
+            var ItemID = IntentRequestExtensions.GetSlotValue(intentRequest, slotName);
+            var success = intentRequest.Intent.Slots.TryGetValue(slotName, out Slot slot);
+            ItemID.Should().Equals(slot.Value);
+        }
+        [Theory]
+        [InlineData(RequestType.IntentRequest)]
+        public void Can_Determine_Request_Type(string requestType)
+        {
+            //IntentRequest File
+            SkillRequest skillRequest = TestUtilities.EntityFromFile<SkillRequest>(testFilePath, testFile);
+            var intentRequest = (IntentRequest)skillRequest.Request;
+            var input = intentRequest.Type;
+            input.Should().Equals(requestType);
+        }
+        [Theory]
+        [InlineData("itemid")]
+        [InlineData("itemstate")]
+        [InlineData("Fullname")]
+        public void NotFound_SlotValue_For_Intent(string slotValue)
+        {
+            SkillRequest skillRequest = TestUtilities.EntityFromFile<SkillRequest>(testFilePath, testFile);
+            var intentRequest = (IntentRequest)skillRequest.Request;
+            intentRequest.Intent.Slots[slotValue].Value = null;
+            var success = IntentRequestExtensions.GetSlotValue(intentRequest, slotValue);
+            success.Should().Equals(null);
+        }
+    }
+}
